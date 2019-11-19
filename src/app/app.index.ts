5b63e98e6afb1b0336d7cbe0ce623adbe7cbb62d
Bruna Klassmann <bklassmann@gmail.com>
2019-11-19 17:36:24 -0300

    Setting Basic Styles Structure

--- src/app/app.index.ts
+++ src/app/app.index.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.index.html'  
 })
 export class AppComponent {
   title = 'darkcast';
